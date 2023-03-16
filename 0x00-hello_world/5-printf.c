26eacecacf99c3c83bfafb5c881f3ec6e65ae24a
chucky-new <tanithadouglasjennifer12@gmail.com>
2023-03-16 16:40:27 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * Main - print a line of code using printf
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return(0)
+}
