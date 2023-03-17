742ea06914a1928c354ea85909b4daeaf6035fcd
chucky-new <tanithadouglasjennifer12@gmail.com>
2023-03-16 17:18:07 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print sizes of various types
+ *
+ * Return: 0
+ */
+int main(void)
+{ 
+	int a;
+	char b;
+	long c;
+	long long int d;
+	float e;
+	long int f;
+	return (0);
+}
