52e0d24a9b1d9be7a895e561b4cfd001458c4ba3
chucky-new <tanithadouglasjennifer12@gmail.com>
2023-03-17 14:24:51 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,11 +4,11 @@
  *
  * Return: 0
  */
-int main (void)
+int main(void)
 {
 	char letter;
 
-	for(letter = 'a'; letter <= 'z'; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
 	
 	putchar('\n');
