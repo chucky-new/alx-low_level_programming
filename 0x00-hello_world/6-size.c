e9c1a99b1fc74ccaa445abf6423edeaf67f5b600
chucky-new <tanithadouglasjennifer12@gmail.com>
2023-03-16 17:35:21 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,8 +14,8 @@ int main(void)
 	
 	printf("Size of a char: %1u bytes(S)\n", (unsigned long)sizeof (a));
 	printf("Size of a char: %1u bytes(S)\n", (unsigned long)sizeof (b));
-		printf("Size of a char: %1u bytes(S)\n", (unsigned long)sizeof (c));
-		printf("Size of a char: %1u bytes(S)\n", (unsigned long)sizeof (d));
-		printf("Size of a char: %1u bytes(S)\n", (unsigned long)sizeof (f));
-		return (0);
+	printf("Size of a char: %1u bytes(S)\n", (unsigned long)sizeof (c));
+	printf("Size of a char: %1u bytes(S)\n", (unsigned long)sizeof (d));
+	printf("Size of a char: %1u bytes(S)\n", (unsigned long)sizeof (f));
+	return (0);
 }
