619143009431f5bacba75d11ce5316c19a2fd120
chucky-new <tanithadouglasjennifer12@gmail.com>
2023-03-17 15:06:30 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print numbers from 0-10
+ *
+ * Return: 0 (Success)
+ */
+int main(0)
+{
+	int num;
+
+	for (num = 0; num <= 10, num++0);
+
+	printf('\n');
+
+	return (0);
+}
