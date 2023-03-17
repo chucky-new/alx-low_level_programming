e18487bec56a4e198f3c16b693baca3f16f07ecc
chucky-new <tanithadouglasjennifer12@gmail.com>
2023-03-17 15:33:28 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print numbers using char
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char num;
+
+	for (num = 0; num < 10, num++)
+		putchar((num % 10) + '0');
+	
+	putchar('\n');
+
+	return (0);
+}
+
