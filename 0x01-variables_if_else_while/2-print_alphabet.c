84a2fc502bf536791534c4f86872ebe990fcdca2
chucky-new <tanithadouglasjennifer12@gmail.com>
2023-03-17 14:23:22 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints lowercase latters in the alphabet
+ *
+ * Return: 0
+ */
+int main (void)
+{
+	char letter;
+
+	for(letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	
+	putchar('\n');
+
+	return (0);
+}
