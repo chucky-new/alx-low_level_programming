aee391f9c42a1d408018ae90ce7fd56d74585c9c
chucky-new <tanithadouglasjennifer12@gmail.com>
2023-03-20 16:10:41 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - print the alphabet
+ * 
+ * Return: 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+
+	}
+	_putchar('\n');
+}
