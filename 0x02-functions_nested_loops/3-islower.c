b00c83c2388e889c864510642f52f2b3bda542a6
chucky-new <tanithadouglasjennifer12@gmail.com>
2023-03-20 16:47:27 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
  * _islower - return if 1 is lower
+ * @c: the character to be checked
  * Return: 1 if char is lower, unless 0
  */
 int _islower(int c)
 {
-	if (c>= 'a'&&c>= 'z')
+	if (c >= 'a' && c <= 'z')
 		return (1);
 	else
 		return (0);
