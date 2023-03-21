82e1444e45cb83f19cb979dffcb884257490986e
chucky-new <tanithadouglasjennifer12@gmail.com>
2023-03-20 17:32:54 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs(int)- check code
+ *  @n: check the integer
+ *  Return: the value of integer
+ */
+int _abs(int)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
