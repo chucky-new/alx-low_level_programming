c5bbf8cb22e6f3c4299ccc5f3ada2ca67b686c14
chucky-new <tanithadouglasjennifer12@gmail.com>
2023-03-20 17:01:09 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
  * _isalpha(int c) - check code
- * @c: upper or lower case
- * Return: 1 if c is lowercase, otherwise 0
+ * @c: check if character is lower or uppercase
+ * Return: 1 if c is lowercase ro uppercase, otherwise 0
  */
 int _isalpha(int c)
 {
 	if ((c >= 'A' && c <= 'Z')|| 
-			((c >= 'a' && c <= 'z')
+			(c >= 'a' && c <= 'z'))
 			 return (1);
 		else
 	   return (0);
