3b6911b75dafe16d0b407fbacbbf01c72885d04b
chucky-new <tanithadouglasjennifer12@gmail.com>
2023-03-20 18:53:07 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,8 +6,9 @@
  */
 int _isalpha(int c)
 {
-	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+	if ((c >= 'a' && c <= 'z') ||
+	    (c >= 'A' && c <= 'Z'))
 		return (1);
-	else 
-	return (0);
+	else
+		return (0);
 }
