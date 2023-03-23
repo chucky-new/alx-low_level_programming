60556b89da0a2cff338bb0c60bd037b5466fc16a
chucky-new <tanithadouglasjennifer12@gmail.com>
2023-03-23 16:37:36 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,21 +2,18 @@
 /**
  * print_most_numbers - checks code for 0-9
  *
- * Return: 0-9 except for 2 and 4
+ * Return: 0-9; excluding 2,4
  */
 void print_most_numbers(void)
 {
 	int n;
 
-	for (n >= '0'; n <= '9'; n++);
+	for (n = 0; n <= 9; n++)
 	{
-		if (n != '2')
-		{
-			if (n != '4')
-			{
-				_putchar(n);
-			}
-		}
+		if ((n == 2) || (n == 4))
+			continue;
+		else
+			_putchar(n + '0');
 	}
 	_putchar('\n');
 }
