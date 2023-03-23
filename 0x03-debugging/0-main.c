755f780fd8588176e02979efc4a0be9243ef9f16
chucky-new <tanithadouglasjennifer12@gmail.com>
2023-03-23 14:18:22 -0700

    Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main -test code if integer is postive or negative
+ * Return: 0 
+ */
+int main(void)
+{
+	int i;
+
+	i = 98;
+	postive_or_negative(i);
+
+	return (0):
+}
